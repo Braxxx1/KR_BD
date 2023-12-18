39f516323ceab9bda3021b838bf54758222f13ff
Braxxx1 <50825640+Braxxx1@users.noreply.github.com>
2023-12-18 11:45:38 +0300

    End programm

--- Program.cs
+++ Program.cs
@@ -1,6 +1,25 @@
-﻿string wordItems = Console.ReadLine();
-string[] wordSpis = wordItems.Split(" ");
+﻿string[] wordSpis = Console.ReadLine().Split(" ");
+int size = 0;
 foreach (var item in wordSpis)
+{
+    if (item.Length <= 3)
+    {
+        size += 1;
+    }    
+}
+
+string[] newWordSpis = new string[wordSpis.Length];
+int index = 0;
+foreach (var item in wordSpis)
+{
+    if (item.Length <= 3)
+    {
+        newWordSpis[index] = item;
+        index++;
+    }    
+}
+
+foreach (var item in newWordSpis)
 {
     Console.WriteLine(item);
 }
